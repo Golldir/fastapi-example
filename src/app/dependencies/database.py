c3b042354c9e2c76274ba051c225b8a7052145af
maxim <golldirmax123@gmail.com>
2025-10-15 00:28:50 +0500

    init

--- src/app/dependencies/database.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Request
-from src.app.core.database import DatabaseConnection
-
-def get_database_connection(request: Request) -> DatabaseConnection:
-    """FastAPI автоматически передает Request сюда!"""
-    return request.app.state.database
